4c2227c2abe6814607c94b48357d1304c504794e
Mih-gif <mihailgoo7@yandex.ru>
2025-02-04 23:12:11 +0300

    добавлены файлы hello.php и start.bat

---
+++ Task01/hello.php
@@ -0,0 +1,9 @@
+<?php
+    date_default_timezone_set('Europe/Moscow');
+
+    $currentDateTime = date('Y-m-d H:i:s');
+    $fullName = 'Вотяков Михаил';
+
+    echo "Текущая дата и время: $currentDateTime\n";
+    echo "Фамилия и имя: $fullName\n";
+    
